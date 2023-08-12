78e92469e6d1ae086bec6f0bc6ba828a097d0fd6
LeeWest89 <6683@holbertonstudents.com>
2023-08-11 20:58:22 -0700

    Add NULL stuff to 1

--- testing/1-insertion_sort_list.c
+++ testing/1-insertion_sort_list.c
@@ -12,6 +12,9 @@ void insertion_sort_list(listint_t **list)
 	listint_t *c, *i, *temp;
 	int v;
 
+	if (list == NULL || (*list) == NULL || (*list)-> == NULL)
+		return;
+
 	c = (*list)->next;
 
 	while (c)
